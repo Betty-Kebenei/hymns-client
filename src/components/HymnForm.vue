0a73cba3562dcc8234147bf226c819a81bc1efb3
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-09-14 14:45:21 +0300

    GET all hymns and display them to the user.

--- src/components/HymnForm.vue
+++ src/components/HymnForm.vue
@@ -1,6 +1,10 @@
 <template>
  <div>
-  <p> this is a form </p>
+    <form @submit.prevent="addHymn">
+        <input type="text" placeholder="Title of the hymn..." v-model="title" >
+        <textarea v-model="stanzas" placeholder="Add the stanzas of the hymn..."></textarea>
+        <input type="submit" value="Add Hymn">
+    </form>
  </div>
 </template>
 
@@ -11,4 +15,32 @@ export default {
 </script>
 
 <style scoped>
+
+input {
+    width: 100%;
+    padding: 15px;
+    margin-bottom: 10px;
+    font-size: 1.3em;
+    border-radius: 3px;
+}
+
+textarea {
+    width: 100%;
+    padding: 15px;
+    min-height: 500px;
+    padding-bottom: 0;
+    font-size: 1.3em;
+    border-radius: 3px;
+}
+
+input[type="submit"] {
+    width: 20%;
+    float: right;
+    margin-top: 20px;
+    background-color: #E0EDF4;
+    font-weight: bold;
+    color: #000;
+    border-radius: 3px;
+}
+
 </style>
