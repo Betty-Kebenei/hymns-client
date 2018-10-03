d203f92e35c4871facf3d0d3ed235386729b45bc
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-10-03 13:28:40 +0300

    Add a fixed header and some styling.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Hymns</router-link>
-      <router-link to="/hymn-form">Add Hymn</router-link>
-    </nav>
-
+    <header>
+      <img src="./assets/logo.png" alt="logo" />
+      <nav>
+        <router-link to="/">Hymns</router-link>
+        <router-link to="/hymn-form">Add Hymn</router-link>
+      </nav>
+    </header>
     <router-view />
   </div>
 </template>
@@ -16,38 +18,41 @@ import HymnForm from './components/HymnForm.vue';
 export default {
   name: 'app',
   components: {
-    Hymns,
-    HymnForm
+    'hymns': Hymns,
+    'hymn-form': HymnForm
   }
 }
 </script>
 
 <style>
- 
-body {
-  background-color: #EEEEEE;
-  font-family: 'Montserrat', sans-serif;
-  display: grid;
-  grid-template-rows: auto;
-  justify-items: center;
-  padding-top: 50px;
-}
-
-body, html {
-  margin: 0;
-  height: 100%;
-}
-
 #app {
-  width: 90%;
+  width: 100%;
+  left: -10%;
+}
+
+header {
+  background-color: #fff;
+  box-shadow: 1px 0 1px rgba(0,0,0,0.25);
+  width: 100%;
+  position: fixed;
+  top: 0;
+  max-height: 80px;
+  margin-bottom: 5px;
+}
+
+header img {
+  height: 80px;
+  width: 150px;
+  padding: 0 20px;
+}
+
+header img, nav {
+  display: inline;
 }
 
 nav {
-  padding: 20px 20px 20px 0;
-}
-
-nav {
-  padding: 20px 20px 20px 0;
+  padding: 30px 20px 20px 0;
+  position: absolute;
 }
 
 nav a {
@@ -58,5 +63,11 @@ nav a {
   color: rgb(0, 110, 255);
   font-weight: bold;
   margin-right: 15px;
+}
+
+body {
+  padding-top: 20px;
+  margin-top: 3.5%;
+  background:  url("./assets/music.png") repeat-y 80% top fixed;
 }
 </style>
